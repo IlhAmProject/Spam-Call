0747baf43e0688dc247c6d3d5afa87e0f7970ad7
IlhAmProject <106818575+IlhAmProject@users.noreply.github.com>
2022-08-19 12:33:19 +0700

    Add files via upload

---
+++ module.sh
@@ -0,0 +1,17 @@
+pkg install python bash git
+pkg install python2
+pkg install figlet
+pkg install ruby
+pkg install  bash
+gem install lolcat
+pkg install bastet &> //dev/null
+pkg install vitetris &> //dev/null
+pkg install nsnake &> //dev/null
+pkg install moon-buggy &> //dev/null
+pkg install ninvaders &> //dev/null
+echo "Package Already Installed"
+pip install requests colorama bs4
+pip install requests colorama bs4
+echo "pip Already Installed"
+sleep 3
+python main.py
